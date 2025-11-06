bec9435127d4576a9b510067cf3125a25346d33a
Lodge <alessio.lodge@tno.nl>
2025-11-05 18:13:14 -0800

    AA more work

---
+++ aaa.py
@@ -0,0 +1,13 @@
+import sys
+import streamlit
+import plotly
+import pandas as pd
+import numpy as np
+import pulp
+
+print(f"Python version: {sys.version}")
+print(f"streamlit=={streamlit.__version__}")
+print(f"plotly=={plotly.__version__}")
+print(f"pandas=={pd.__version__}")
+print(f"numpy=={np.__version__}")
+print(f"pulp=={pulp.__version__}")
